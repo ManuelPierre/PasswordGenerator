9b44f1d91a4172b584e7210e944acbfe5c7e7a45
Manuel Pierre <pierrem1.mp@gmail.com>
2020-11-26 22:07:51 -0500

    added character validation

---
+++ script.js
@@ -0,0 +1,148 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+function generatePassword() {
+  // get choices from user and return password
+  var upperAlpha = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  var lowerAlpha = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+
+  var numb = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  var specChar = [
+    "!",
+    "@",
+    "$",
+    "%",
+    "^",
+    "&",
+    "*",
+    "(",
+    ")",
+    "_",
+    "+",
+    "=",
+    "[",
+    "]",
+    "'",
+    "<",
+    ">",
+    "?",
+    "/",
+  ];
+
+  var length = parseInt(
+    prompt("Specify the number of characters your password should have")
+  );
+
+  if (isNaN(length) === true) {
+    console.log("test");
+    alert("Password requires a number");
+    return;
+  }
+
+  if (length < 8) {
+    alert("Password required to be no less than 8 characters");
+    return;
+  }
+
+  if (length > 128) {
+    alert("Password length must be no more than 128 characters");
+    return;
+  }
+
+  var reqNum = confirm("Select Ok to fulfill requirements of Numbers");
+  var reqLowerCase = confirm(
+    "Select Ok to fulfill requirements of having a Lower case Letter!"
+  );
+  var reqSpecChar = confirm(
+    "Select Ok to fulfill requirements of special characters!"
+  );
+  var reqUpperCase = confirm(
+    "Select Ok to fulfill requirements of having a upper case letter!"
+  );
+
+  if (
+    reqNum === false &&
+    reqLowerCase === false &&
+    reqSpecChar === false &&
+    reqUpperCase === false
+  ) {
+    alert(
+      "Your password must include atleast one number, lowwercase letter, uppercase letter, and a special character!"
+    );
+    return;
+  }
+
+  var passwordText = {
+    length: length,
+    reqLowerCase: reqLowerCase,
+    reqNum: reqNum,
+    reqSpecChar: reqSpecChar,
+    reqUpperCase: reqUpperCase,
+  };
+  console.log(passwordText);
+  return passwordText;
+}
